8c611abcee2daf17275b511ecbea375224ec919f
justyntyrrell <73371394+justyntyrrell@users.noreply.github.com>
2020-11-02 19:32:14 -0500

    Update findClosestCentroids.m

--- findClosestCentroids.m
+++ findClosestCentroids.m
@@ -8,38 +8,21 @@ function idx = findClosestCentroids(X, centroids)
 % Set K
 K = size(centroids, 1);
 
-% You need to return the following variables correctly.
 idx = zeros(size(X,1), 1);
 listJ = zeros(K, 1);
+
+
 for i = 1 : size(X, 1)
   for j = 1: K
     computeJ = (norm(X(i, :)- centroids(j, :)))^2;
     listJ(j) = computeJ;
   
 end
+
 [num, ind] = min(listJ);
 idx(i) = ind;
 end
 
-
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Go over every example, find its closest centroid, and store
-%               the index inside idx at the appropriate location.
-%               Concretely, idx(i) should contain the index of the centroid
-%               closest to example i. Hence, it should be a value in the 
-%               range 1..K
-%
-% Note: You can use a for-loop over the examples to compute this.
-%
-
-
-
-
-
-
-
-% =============================================================
 
 end
 
