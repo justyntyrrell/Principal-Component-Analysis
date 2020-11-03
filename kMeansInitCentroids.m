198c335ce59ada71ebe4b0beeeec5995652bdf9e
justyntyrrell <73371394+justyntyrrell@users.noreply.github.com>
2020-11-02 19:32:49 -0500

    Update kMeansInitCentroids.m

--- kMeansInitCentroids.m
+++ kMeansInitCentroids.m
@@ -11,20 +11,6 @@ centroids = zeros(K, size(X, 2));
 % Randomly reorder the indices of examples
 randidx = randperm(size(X, 1));
 % Take the first K examples as centroids
-centroids = X(randidx(1:K), :);
-% ====================== YOUR CODE HERE ======================
-% Instructions: You should set centroids to randomly chosen examples from
-%               the dataset X
-%
-
-
-
-
-
-
-
-
-% =============================================================
 
 end
 
